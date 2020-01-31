601ac02b384c9c235c017e5f3c365c0fe78fdb1c
mehdi <mehdi.behjati048@gmail.com>
2020-01-31 11:05:15 +0330

    Change10

--- app/src/main/java/com/savar/myapplication/RecyclerActivity.java
+++ app/src/main/java/com/savar/myapplication/RecyclerActivity.java
@@ -1,6 +1,9 @@
 package com.savar.myapplication;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,6 +17,18 @@ public class RecyclerActivity extends AppCompatActivity implements RecyclerClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         RecyclerView recycler = findViewById(R.id.recycler);
 
